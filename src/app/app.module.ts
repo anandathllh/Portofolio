ca543f39f6fb07aa821ec72670af42e70b9239ff
ansalma <anandaslm04@gmail.com>
2024-05-16 15:32:40 +0700

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { HomeComponent } from './pages/home/home.component';
     HeaderComponent,
     FooterComponent,
     MainLayoutComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
